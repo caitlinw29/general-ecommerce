9a2756dbec0f4853d3991d7642aa94aa06126df1
Caitlin Wall <caitlinwall96@gmail.com>
2022-01-29 10:54:27 -0500

    Add many-to-many through ProductTag model

--- models/index.js
+++ models/index.js
@@ -16,8 +16,24 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'products'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tags'
+});
 
 module.exports = {
   Product,
